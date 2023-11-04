1f67eb3256ea828382b6b3deaa6203770c58c5c0
Ivan <ijvv06242001@gmail.com>
2023-11-04 14:01:06 +0100

    completado ejercicio

--- Practica1Trimestre/MiniCalculadora/MiniCalculadora.php
+++ Practica1Trimestre/MiniCalculadora/MiniCalculadora.php
@@ -1,29 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <h1>Mini Calculadora</h1>
+<?php
+include("MiniCalculadora.html");
 
-    <form action="">
-        <label for="n1">1º Numero:</label><input type="number" id="n1">
-        <br>
-        <label for="n2">2º Numero:</label><input type="number" id="n2">
-        <div>
-            <button type="submit">+</button>
-            <button type="submit">-</button>
-            <button type="submit">*</button>
-            <button type="submit">/</button>
-        </div>
-        <br>
-        <div>
-            <input type="radio" name="decimal" id="deci"> <label for="deci">Decimal</label> 
-            <input type="radio" name="binaria" id="bina"> <label for="bina">Binaria</label>
-            <input type="radio" name="hexadecimal" id="hexa"> <label for="hexa">HexaDecimal</label> 
-        </div>
-    </form>
-</body>
-</html>
+if(!isset($_POST['numero1'])|| !isset($_POST['numero2'])|| !isset($_POST['conversion'])){
+    echo "Hay que rellenar todos los campos";
+} else {
+    //Datos Formulario
+    $n1=$_POST['numero1'];
+    $n2=$_POST['numero2'];
+    $operador=$_POST['operador'];
+    $conversion=$_POST['conversion'];
+
+   if ($n2==0&&$operador=='/'){
+    echo "No se puede dividir con 0";
+    return;
+   } 
+
+   $resultado= calcular($n1,$operador,$n2);
+
+   switch ($conversion){
+    case 'decimal' : echo "El resultado en Decimal es : ".$resultado;break;
+    case 'binario' : echo "El resultado en Binario es : ".decbin($resultado);break;
+    case 'hexadecimal' : echo "El resultado en HexaDecimal es : ".dechex($resultado);break;
+   }
+
+
+}
+
+
+//Funciones
+function calcular($num1,$oper,$num2){
+    $resu=0;
+    switch ($oper){
+        case '+':$resu= $num1+$num2;break;
+        case '-':$resu= $num1-$num2;break;
+        case '*':$resu= $num1*$num2;break;
+        case '/':settype($resu,"float");$resu= $num1/$num2;break;
+    }
+    return $resu;
+}
+
+?>
